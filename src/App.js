17608fa2d1456bc07f5dfd080a12f7f4ed8d6999
telix <telegonicaxx@live.com>
2020-09-01 17:27:31 -0700

    checkpoint

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { dic } from "./dictionary";
 
 function App() {
+
+	const data ={
+			greeting : "React",
+			title : "Dictionary",
+	}
+
+	function greet(){
+		return data.greeting + " " + data.title 
+	}
+
+		//for each of the keys 
+		//	print the key 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+			<div>
+				<h1>{greet()}</h1>
+				<label htmlFor="search">Search: </label>
+				<input id="search" type="text"/>
+				<hr/>
+				<List/>
+			</div>
   );
+}
+
+function List() {
+	return (
+			<div> 
+			{dic.map((item, i) => { return (
+						<div key={i}>
+						<a href={item.url}>{item.word}</a>
+						<p>{ Object.entries(item.definitions) }</p>
+						<hr/>
+						</div>
+			)})}
+			</div>
+	)
 }
 
 export default App;
