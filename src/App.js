abb35dd7213820c17abfa4ecca36d3ef6dad5003
telix <telegonicaxx@live.com>
2020-09-02 18:59:14 -0700

    rewritten using advance destructuring

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { dict } from "./dictionary";
 
 function App() {
 
-	const [searchTerm, setSearchTerm] = React.useState('');
+	const [searchTerm, setSearchTerm] = React.useState('react');
 
 	const handleChange = change => setSearchTerm(change.target.value);
 	
@@ -24,34 +24,36 @@ function App() {
   return (
 			<div>
 				<h1>{greet()}</h1>
-				<Search onSearch={handleChange}/>
+				<Search search={searchTerm} onSearch={handleChange}/>
 				<hr/>
 				<List dict={searchedDict}/>
 			</div>
   )
 }
 
-function Search(props){
-
+function Search( {search, onSearch} ){
+		/*fuction for returning search component*/
 	return (
 		<div>
 			<label htmlFor="search">Search: </label>
-			<input id="search" type="text" onChange={props.onSearch}/>
+			<input id="search" type="text" value={search} onChange={onSearch}/>
 		</div>
 	)
 }
 
+//for every Item in the list pass the Itme into an Item component 
+const List = ({dict}) => dict.map( (item, iter) => <Item key = {iter} item={item}/> )
 
-function List(props){
+
+
+
+const Item = ({item}) => {
+	/* item fuction for rendering a single word*/
 	return (
 			<div> 
-			{props.dict.map(
-					(item, i) =>  
-						<div key={i}>
-							<a href={item.url}>{item.word}</a>
-							<p>{Object.entries(item.definitions)}</p>
-							<hr/>
-						</div> )}
+				<a href={item.url}>{item.word}</a>
+				<p>{Object.entries(item.definitions)}</p>
+				<hr/>
 			</div>
 	)
 }
